df0c6ce8293137b71def7f538b4a59407bff4eb7
Richard Matic <richardbmatic17@gmail.com>
2021-02-23 23:27:50 +0800

    added created_at and updated_at in model

--- src/app/models/images.js
+++ src/app/models/images.js
@@ -12,7 +12,19 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.BOOLEAN,
       defaultValue: 0,
     },
-  }, {});
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)'),
+      field: 'created_at',
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)'),
+      field: 'updated_at',
+    },
+  }, {
+    timestamps: false,
+  });
   images.associate = function(models) {
     // associations can be defined here
   };
