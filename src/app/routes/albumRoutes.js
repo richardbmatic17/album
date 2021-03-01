1b66bc3f18e1c614eadce39709d77d9c8b4c935c
Richard Matic <richardbmatic17@gmail.com>
2021-03-02 02:29:48 +0800

    partial commit for file upload

--- src/app/routes/albumRoutes.js
+++ src/app/routes/albumRoutes.js
@@ -1,5 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const multer  = require('multer');
+const fs = require('fs');
+const fsPromises = fs.promises
 const { middleware } = require('../lib');
 const {
   getAlbums,
@@ -9,7 +12,30 @@ const {
   deleteAlbum,
 } = require('../controllers/albumControllers');
 
+// SET STORAGE
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const directory = `${__dirname}/../../uploads/${file.fieldname}`;
+console.log(`\n\n\n req : ${JSON.stringify(req.params)} \n\n\n`);
+
+    if (!fs.existsSync(directory)){
+      fs.mkdirSync(directory);
+    }
+
+    cb(null, directory)
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage });
+
 router.use(middleware.routerLogger);
+
+router.post('/:albumId/images', upload.array('images', 20), function (req, res) {
+  res.send(req.files)
+});
 
 router.get('/:id', getAlbum);
 
