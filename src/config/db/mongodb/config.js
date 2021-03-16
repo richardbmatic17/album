5e976112183090f2355a080382ef142a7b4c2607
Richard Matic <richardbmatic17@gmail.com>
2021-03-16 21:55:01 +0800

    added mongodb

---
+++ src/config/db/mongodb/config.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "database": {
+      "url": "mongodb://localhost/album",
+      "options": {
+        "useNewUrlParser": true,
+        "useUnifiedTopology": true
+      }
+    }
+  },
+  "test": {
+    "database": {
+      "url": "mongodb://localhost/mongoose_test",
+      "options": {
+        "useNewUrlParser": true
+      }
+    }
+  },
+  "production": {
+    "database": {
+      "protocol": "mongodb",
+      "username": "root",
+      "password": "password",
+      "name": "database_production",
+      "host": "localhost",
+      "port": "",
+      "options": {
+        "useNewUrlParser": true
+      }
+    }
+  }
+}
