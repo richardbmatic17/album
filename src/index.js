99d66f4bd8230890bbff91c54f53678894e37751
Richard Matic <richardbmatic17@gmail.com>
2021-03-17 01:50:51 +0800

    added extended in urlencoded

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.disable("x-powered-by");
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
 app.use(express.json());
